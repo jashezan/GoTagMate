7ff307d2a37d03665ec5d020891f2a10a6193625
Jubyer Ahmed Shezan <84038649+jashezan@users.noreply.github.com>
2025-03-21 08:20:52 +0600

    feat(Gin): Added Gin Binding tags

---
+++ src/tags/gin_binding.tag.ts
@@ -0,0 +1,22 @@
+import type { ITag } from "./tag.type";
+import { fetchValidateKeywords } from "./validate.tag";
+
+const tag_list =
+	fetchValidateKeywords.tag_list?.map((tag) => ({
+		tag_name: tag.tag_name,
+		tag_description: tag.tag_description,
+		tag_usage:
+			tag.tag_usage?.map((usage) =>
+				usage.replace("validate:", "binding:"),
+			) || [],
+	})) || null;
+
+export const FetchGinBindingKeywords: ITag = {
+	keyword: "binding",
+	description:
+		"Gin bindings are used to serialize JSON, XML, path parameters, form data, etc. to structs and maps. Gin uses go-playground/validator/v10 for validation.",
+	library_link: ["github.com/gin-gonic/gin"],
+	version: "v1.10.0",
+	last_published: "May 7, 2024",
+	tag_list: tag_list,
+};
