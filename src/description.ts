3ac9d548a41a3565e2cbdce47b6f56a1d10c7d82
Jubyer Ahmed Shezan <84038649+jashezan@users.noreply.github.com>
2025-03-21 07:12:30 +0600

    chore: Removed console.log about the app at start

--- src/description.ts
+++
@@ -1,9 +0,0 @@
-/** Name of the extension */
-export const APP_NAME = "GoTagMate";
-
-/** Version of the extension */
-export const APP_VERSION = "0.0.1";
-
-/** Description of the extension */
-export const APP_DESCRIPTION =
-	"GoTagMate - Autocompletion and hover suggestions for Go struct tags.";
--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,6 @@
 import * as vscode from "vscode";
 import { GoTagCompletionProvider } from "./completion/completion.provider";
 import { TRIGGER_CHARACTERS } from "./constants/trigger_chars.const";
-import { APP_DESCRIPTION, APP_NAME, APP_VERSION } from "./description";
 
 export function activate(context: vscode.ExtensionContext) {
 	const provider = new GoTagCompletionProvider();
@@ -14,9 +13,6 @@ export function activate(context: vscode.ExtensionContext) {
 			...TRIGGER_CHARACTERS,
 		),
 	);
-
-	// Log activation for debugging
-	console.log(`${APP_NAME} v${APP_VERSION} activated: ${APP_DESCRIPTION}`);
 }
 
 /**
